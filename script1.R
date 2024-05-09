9ef73f9bfe9279e35ec63316e617c115680ab607
Heini Järviö <heinimjarv@gmail.com>
2024-05-09 17:39:33 +0200

    Scraping code

--- script1.R
+++ script1.R
@@ -37,12 +37,15 @@ rd <- rsDriver(browser = "firefox",
 
 driver <- rd$client
 
+#setting the window size as phone adapted website is easier to manage
+
 driver$setWindowSize(360, 640)
 
 #closing the driver
 
 driver$close()
 
+#navigating to the website with the selenium driver
 driver$navigate('https://www.psychologie.ch/en/psyfinder-map')
 
 html <- driver$getPageSource()[[1]]
@@ -50,8 +53,25 @@ print(html)
 
 pageEl  <- driver$findElements(using = "css", value = "a")
 
+#clicking open the button to show all the results
 button_element <- driver$findElement(using = "class name", "d-md-none")
 button_element$clickElement()
+
+#scrolling to the bottom of the page to load all the results
+scroll_element <- driver$findElement(using = "class name", "overflow-scroll")
+scroll_element$sendKeysToElement(list(key = "end"))
+
+#creating a loop that does the infine scroll
+
+for(i in 1:360){
+  scroll_element$sendKeysToElement(list(key = "end"))
+  Sys.sleep(5)
+}
+
+# Get the HTML source and save to a file
+html <- driver$getPageSource()[[1]]
+writeLines(html, "psyfinder.html")
+
 
 #scraping each page 
 
