b152bf32feb96433d62bbd5de3f3d775cf06da8f
Heini Järviö <heinimjarv@gmail.com>
2024-05-10 15:26:43 +0200

    trying to scrape info from the links

--- scraping.R
+++ scraping.R
@@ -16,56 +16,49 @@ links[1:60]
 
 links[3700:3796]
 
-#profile links start at 56
+#profile links start at 56 and end at 3793
 
 links_profiles <- links[56:3793]
 
+profile_list <- vector(mode="list", length=length(links_profiles))
 
 #2. extract the links
 
-#3. store the links in a vector
-
-profile_list <- vector(mode="list", length=length(links_profiles))
+write.csv(links_profiles, "profilelinks.csv", row.names = FALSE)
 
 length(links_profiles)
 
 #lenght is the same as n of psychologist jubii!!
 
+
+#3. store the links in a vector
+
+read_html(links_profiles[1]) %>%
+  html_elements(css=".pe-8px") %>%
+  html_text()
+
+?paste0
+
+
 #4. loop over the links and scrape the content
 
-?str_c
-
-links2 <- str_c("psyfinder.html", links_profiles)
-
-articles <- vector(mode="list", length=length(links2))
-
-for(i in 1:length(links2)){
+for(i in 1:length(links_profiles)){
   cat("iteration", i, "\n")
-  articles[[i]] <- read_html(links2[i]) %>%
-    html_elements(css="a") %>%
-    html_attr(name="href")
+  links_profiles[[i]] <- read_html(links_profiles[u]) %>%
+    html_elements(css=".pe-8px") %>%
+    html_text()
   Sys.sleep(8)
 }
+
+
+
+
+
 
 #5. store the content in a list
 
 #6. save the list as a csv file
 
 
-links2[10]
-
-read_html("https://www.psychologie.ch/en/psyfinder-map?search=") %>%
-  html_elements(css=css.selector) %>%
-  html_text()
-
-#getting the links
-read_html("https://www.psychologie.ch/en/psyfinder-map?search=") %>%
-  html_elements(css="a") %>%
-  html_text()
-
-#getting the attributes
-links <- read_html("https://www.psychologie.ch/en/psyfinder-map?search=") %>%
-  html_elements(css="a") %>%
-  html_attr(name="href")
 
 
