7e721440f2438591f4455a1e596901d5339f3713
Heini Järviö <heinimjarv@gmail.com>
2024-05-13 21:48:19 +0200

    corrected the loop to find correct data

--- scraping.R
+++ scraping.R
@@ -7,8 +7,8 @@ library(httr)
 #1. read the html file
 
 links <- read_html("psyfinder.html") %>%
-  html_elements(css="a") %>%
-  html_attr(name="href")
+  html_elements(css = "a") %>%
+  html_attr(name = "href")
 
 #checking where the profile links start and end
 
@@ -20,7 +20,7 @@ links[3700:3796]
 
 links_profiles <- links[56:3793]
 
-profile_list <- vector(mode="list", length=length(links_profiles))
+profile_list <- vector(mode = "list", length = length(links_profiles))
 
 #2. extract the links
 
@@ -35,21 +35,21 @@ length(links_profiles)
 
 # reading the first profile on the list to form a basis for a loop
 
-page <- httr::GET(links_profiles[3],
-                        httr::add_headers(
-                          From = "heini.jaervioe@stud.unilu.ch",
-                          `User-Agent` = R.Version()$version.string))
+page <- httr::GET(
+  links_profiles[3],
+  httr::add_headers(From = "heini.jaervioe@stud.unilu.ch", `User-Agent` = R.Version()$version.string)
+)
 
 bin <- content(page, as = "parsed")
 
-writeBin(object = bin, con ="testpage.html")
+writeBin(object = bin, con = "testpage.html")
 
 #reading the needed information from the first profile in the html file
 
 
 titles <- read_html(here::here("testpage.html")) %>%
   html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "align-items-start", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "border-color-pumpkin-500", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "body-4", " " ))]//div') %>%
-  html_text2( ) %>%
+  html_text2() %>%
   strsplit("\n")
 
 #checking where the needed information is located in the list
@@ -78,10 +78,18 @@ write.csv(psychologist_df, "psychologist.csv", row.names = FALSE)
 links_profiles[1:5]
 
 
-read_html(links_profiles[3]) %>%
-  html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "align-items-start", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "border-color-pumpkin-500", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "body-4", " " ))]//div') %>%
+test <- read_html(links_profiles[1]) %>%
+  html_elements(xpath = '//h3[.="Address"]/parent::div') %>%
   html_text2() %>%
-  strsplit("\n")
+  str_replace("Address\n", "") %>%
+  str_replace_all("\n", " ")
+
+# Extract the zipcode of 4 digits from the address string
+zipcode <- str_extract(test, "\\d{4}")
+
+zipcode
+
+test[[1]][1]
 
 print(links_profiles)
 
@@ -96,55 +104,89 @@ links_profiles[3]
 #turning links_profiles into a character vector
 psychologist_combined <- data.frame(Title = character(), stringsAsFactors = FALSE)
 
-for (i in links_profiles) {
-      pages <- GET(i, add_headers(
-                    From = "heini.jaervioe@stud.unilu.ch",
-                    'User-Agent' = R.Version()$version.string))
-      if (status_code(pages) == 200) {  # Check if the request was successful
-        print("Success!")
-      } else {
-        print("Error!")
-        print(status_code(pages))
-      }
-      if (i == 10) break
-      Sys.sleep(8)
-                   
-                      
-bin <- content(pages, as = "parsed")
-                   
-profiles <- bin %>%
-html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "align-items-start", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "border-color-pumpkin-500", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "body-4", " " ))]//div') %>%
-html_text2() %>%
-strsplit("\n")
+i <- 0
+for (profile_link in links_profiles) {
+  i <- i + 1
+  pages <- GET(
+    profile_link,
+    add_headers(From = "heini.jaervioe@stud.unilu.ch", 'User-Agent' = R.Version()$version.string)
+  )
+  if (status_code(pages) == 200) {
+    # Check if the request was successful
+    print("Success!")
+  } else {
+    print("Error!")
+    print(status_code(pages))
+  }
+  if (i == 10)
+    break
+  Sys.sleep(8)
+  
+  
+  bin <- content(pages, as = "parsed")
+  
+  availability <- bin %>%
+    html_elements(xpath = '//div[.="Availability"]/parent::div') %>%
+    html_text2() %>%
+    strsplit("\n")
+  
+  group <- bin %>%
+    html_elements(xpath = '//div[.="Target groups"]/parent::div') %>%
+    html_text2() %>%
+    strsplit("\n")
+  
+  bill <- bin %>%
+    html_elements(xpath = '//div[.="Billing"]/parent::div') %>%
+    html_text2() %>%
+    strsplit("\n")
+  
+  lang <- bin %>%
+    html_elements(xpath = '//div[.="Languages"]/parent::div') %>%
+    html_text2() %>%
+    strsplit("\n")
+  
+  addr <- bin %>%
+    html_elements(xpath = '//h3[.="Address"]/parent::div') %>%
+    html_text2() %>%
+    str_replace("Address\n", "") %>%
+    str_replace_all("\n", " ")
+  
+  zipcode <- str_extract(addr, "\\d{4}")
 
-data <- list(
-  address = profiles[[1]][1],
-  adress2 = profiles[[2]][1],
-  placeholder2 = profiles[[2]][2],
-  placeholder3 = profiles[[3]][2],
-  availability = profiles[[4]][2],
-  placeholder5 = profiles[[5]][2],
-  placeholder6 = profiles[[6]][2],
-  placeholder7 = profiles[[7]][2],
-  groups = profiles[[8]][2],
-  languages = profiles[[9]][2],
-  billing = profiles[[10]][2]
-)
-
-
-psychologist_df <- data.frame(data)
-
-psychologist_combined <- rbind.data.frame(psychologist_combined, psychologist_df)
+  
+  data <- list(
+    address = addr[[1]][1],
+    availability = availability[[1]][2],
+    groups = group[[1]][2],
+    languages = lang[[1]][2],
+    billing = bill[[1]][2],
+    zipcode = zipcode
+  )
+  
+  
+  psychologist_df <- data.frame(data)
+  
+  psychologist_combined <- rbind.data.frame(psychologist_combined, psychologist_df)
 }
-         
 
-#Found out that the structure is different on each page; sos 
+?content
+
+class(bin)
+
+#Found out that the structure is different on each page; sos
 
 write.csv(psychologist_combined, "psychologist.csv", row.names = FALSE)
 
+read_html(bin) %>%
+  html_elements(xpath = '//div[.="Availability"]/parent::div') %>%
+  html_text2() %>%
+  strsplit("\n")
 
+class(bin)
+profiles <- bin %>%
+  html_text2() %>%
+  strsplit("\n")
 
+profiles
 
-
-
-
+bin
