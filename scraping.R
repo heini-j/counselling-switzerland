fbce21e614772a1c699fe023e0daf2bb47b573b3
Heini Järviö <heinimjarv@gmail.com>
2024-05-13 09:57:19 +0200

    fixed code for extracting the right information

--- scraping.R
+++ scraping.R
@@ -1,16 +1,16 @@
 library(rvest)
 library(tidyverse)
-library(RSelenium)
-library(wdman)
+library(httr)
 
 #scraping the links from the stored html file
-
 
 #1. read the html file
 
 links <- read_html("psyfinder.html") %>%
   html_elements(css="a") %>%
   html_attr(name="href")
+
+#checking where the profile links start and end
 
 links[1:60]
 
@@ -26,29 +26,14 @@ profile_list <- vector(mode="list", length=length(links_profiles))
 
 write.csv(links_profiles, "profilelinks.csv", row.names = FALSE)
 
+#checking that the lenght is the same as the number of psychologist on the website
+
 length(links_profiles)
 
-#lenght is the same as n of psychologist jubii!!
+#It was!
 
 
-#3. store the links in a vector
-
-read_html(links_profiles[1]) %>%
-  html_elements(css=".pe-8px") %>%
-  html_text()
-
-?paste0
-
-library(httr)
-
-test <- GET(links_profiles[1])
-
-page <- GET(links_profiles[1],
-            add_headers(
-              From = "heini.jaervioe@stud.unilu.ch",
-              `User-Agent` = user_agent)
-)
-
+# reading the first profile on the list to form a basis for a loop
 
 page <- httr::GET(links_profiles[1],
                         httr::add_headers(
@@ -59,31 +44,35 @@ bin <- content(page, as = "raw")
 
 writeBin(object = bin, con ="testpage.html")
 
+#reading the needed information from the first profile in the html file
+
 titles <- read_html(here::here("testpage.html")) %>%
   html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "align-items-start", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "border-color-pumpkin-500", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "body-4", " " ))]//div') %>%
-  html_text( )
+  html_text2( ) %>%
+  strsplit("\n")
+
+#checking where the needed information is located in the list
 titles
 
-#address
-titles[2]
+#creating a dictionary with the elements we need for the analysis
 
-#availability
-titles[4]
+data <- list(
+  address = titles[[11]][3],
+  availability = titles[[4]][2],
+  groups = titles[[8]][2],
+  languages = titles[[9]][2],
+  billing = titles[[10]][2]
+)
 
-#target groups
-titles[8]
+#turning the dictionary into a dataframe
 
-#languages
-titles[9]
+psychologist_df <- as.data.frame(data)
 
-#billing
-titles[10]
+#saving the data frame as a csv file
 
-?html_table
+write.csv(psychologist_df, "psychologist.csv", row.names = FALSE)
 
-
-
-#html_nodes#4. loop over the links and scrape the content
+#4. loop over the links and scrape the content
 
 for(i in 1:length(links_profiles)){
   cat("iteration", i, "\n")
